2958fba0650ee35feb3e73e1d448ffec5b24b89c
okapyy <m9_17g_suki@yahoo.co.jp>
2020-06-23 22:12:10 +0900

    画像削除ボタン実装

--- app/assets/javascripts/image-preview.js
+++ app/assets/javascripts/image-preview.js
@@ -1,8 +1,8 @@
 $(function() {
+  var data = new DataTransfer();
+  var fileSet = document.querySelector('input[type=file]')
+  var fileReader = new FileReader();
   $('#image-file').on('change',function() {
-    var data = new DataTransfer();
-    var fileSet = document.querySelector('input[type=file]')
-    var fileReader = new FileReader();
     files = $(fileSet).prop('files')[0];
     $.each(fileSet.files, function(i, file) {
       data.items.add(file)
@@ -22,4 +22,37 @@ $(function() {
       }
     });
   });
+
+  $(document).on("click", ".image-delete__btn", function(){
+    let imageDelete =  $(this).parent().parent();
+    let dataDelete = $(imageDelete).data('id');
+    let dataName = $(imageDelete).data('name');
+    if(fileSet.files.length==1){$('input[type=file]').val(null)
+      data.clearData();
+    } else {$.each(fileSet.files, function(i,input){
+      if(input.name == dataName){
+        data.items.remove(dataDelete -1)
+        }
+    })
+    $("#camera").show();
+  }
+    imageDelete.remove();
+    fileSet.files = data.files
+    })
+  // $('.image-delete__btn').on("click", function() {
+  //   var imageDelete = $(this).parent().parent();
+  //   var dataDelete = $(imageDelete).data('id');
+  //   var dataName = $(imageDelete).data('name');
+  //   if(fileSet.files.length==1){$('input[type=file]').val(null)
+  //     data.clearData();
+  //   } else{$.each(fileSet.files, function(i,input){
+  //     if(input.name == dataName){
+  //       data.items.remove(dataDelete -1)
+  //     }
+  //   })
+  //   $(".container__icon").show();
+  // }
+  //   imageDelete.remove();
+  //   fileSet.files = data.files
+  // });
 })
