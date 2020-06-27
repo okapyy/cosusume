32fc9f0f9815c35d926fafaa590c3de70c99c95c
okapyy <m9_17g_suki@yahoo.co.jp>
2020-06-27 16:38:42 +0900

    ユーザー編集画面作成

--- app/controllers/wizard/registrations_controller.rb
+++ app/controllers/wizard/registrations_controller.rb
@@ -70,6 +70,10 @@ class Wizard::RegistrationsController < Devise::RegistrationsController
     params.require(:personal).permit(:age, :skin_color, :skin_condition)
   end
 
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
