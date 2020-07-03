db32b9c5622bfb90b45f2326cbd367e1a6cd20f9
okapyy <m9_17g_suki@yahoo.co.jp>
2020-07-03 09:27:13 +0900

    item編集（複数画像のプレビューが実装できず）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to item_path(@item.id)
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
   def newitem
   end
 
