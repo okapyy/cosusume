765dff8a48a3d0206fb612758915db886314efd4
okapyy <m9_17g_suki@yahoo.co.jp>
2020-07-06 12:52:08 +0900

    item編集・削除ボタン実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_many :item_images
+  has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images
 
   validates :evaluation, numericality: {less_than_or_equal_to: 5.0}
