81c84d3e44dc8b58755d0a2afe132b4d93857535
okapyy <m9_17g_suki@yahoo.co.jp>
2020-07-10 21:04:16 +0900

    不要な部分を削除

--- app/assets/javascripts/item_image.js
+++ app/assets/javascripts/item_image.js
@@ -3,14 +3,4 @@ $(function() {
     var smallsrc = $(this).attr("src");
     $('#bigimage').attr("src", smallsrc).hide().fadeIn();
   });
-  
-  // const bigPic = document.getElementById('bigimage');
-  // const smallimages = document.getElementById('smallimage');
-
-  // for(let smallimage of smallimages) {
-  //   smallimage.addEventListener('mouseover', () => {
-  //     console.log("yaa");
-  //     bigPic.src = smallimage.src;
-  //   })
-  // };
 })
