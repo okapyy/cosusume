8444b24780f121a6f6fc5cddf5c646e7ca71487f
okapyy <m9_17g_suki@yahoo.co.jp>
2020-07-20 19:28:01 +0900

    コメントアウト

--- app/assets/javascripts/item_edit.js
+++ app/assets/javascripts/item_edit.js
@@ -1,7 +1,7 @@
 $(function() {
-  var preview = $('#image-prev');
-  var images = [];
-  var registeted_images_ids = [];
-  var new_image_file = [];
+  // var preview = $('#image-prev');
+  // var images = [];
+  // var registeted_images_ids = [];
+  // var new_image_file = [];
 
 })
