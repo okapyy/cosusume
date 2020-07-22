ac744f49147d966956a4af4756deda37e4019b12
okapyy <m9_17g_suki@yahoo.co.jp>
2020-07-22 15:51:07 +0900

    js

--- app/assets/javascripts/item_image.js
+++ app/assets/javascripts/item_image.js
@@ -2,6 +2,7 @@ $(function() {
   $('#smallimage').hover(function() {
     var index = $('#smallimage').index(this);
     var smallsrc = $(this).eq(index).find('img').attr("src");
+    className = $(this).parent().attr('class')
     $('#bigimage').attr("src", smallsrc).hide().fadeIn();
   });
 })
