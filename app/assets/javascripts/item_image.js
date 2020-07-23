c308aa2076706544535138c2fdd7f0b85b8ea7d2
okapyy <m9_17g_suki@yahoo.co.jp>
2020-07-23 19:19:50 +0900

    js試行錯誤

--- app/assets/javascripts/item_image.js
+++ app/assets/javascripts/item_image.js
@@ -1,8 +1,19 @@
 $(function() {
-  $('#smallimage').hover(function() {
-    var index = $('#smallimage').index(this);
-    var smallsrc = $(this).eq(index).find('img').attr("src");
-    className = $(this).parent().attr('class')
-    $('#bigimage').attr("src", smallsrc).hide().fadeIn();
-  });
+  const bigPic = document.getElementById('bigPic');
+  const thumbs = document.getElementsById('thumb');
+
+  for(let thumb of thumbs) {
+    thumb.addEventListener('mouseover', () => {
+      bigPic.src = thumb.src;
+    })
+  }
 })
+
+// $(function() {
+//   $('#smallimage').hover(function() {
+//     var index = $('#smallimage').index(this);
+//     var smallsrc = $(this).eq(index).find('img').attr("src");
+//     className = $(this).parent().attr('class')
+//     $('#bigimage').attr("src", smallsrc).hide().fadeIn();
+//   });
+// })
