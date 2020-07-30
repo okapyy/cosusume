2f10b15e84e6571b50c623df36f535cf4a4b574f
okapyy <m9_17g_suki@yahoo.co.jp>
2020-07-30 17:20:16 +0900

    マイグレーションファイル追記

--- db/migrate/20200615122325_create_items.rb
+++ db/migrate/20200615122325_create_items.rb
@@ -8,6 +8,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.string :age
       t.string :skin_color
       t.string :skin_condition
+      t.string :category, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_06_15_122359) do
     t.string "age"
     t.string "skin_color"
     t.string "skin_condition"
+    t.string "category", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
