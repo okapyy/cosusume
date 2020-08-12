9b7325bf8a3009f84fcc8f7b6f1c2a5806c9fd6f
okapyy <m9_17g_suki@yahoo.co.jp>
2020-08-12 18:46:04 +0900

    フェイスパウダーページ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,6 +46,10 @@ class ItemsController < ApplicationController
     @items = Item.where(category: "コンシーラー").page(params[:page]).order("created_at DESC").per(25)
   end
 
+  def facepowder
+    @items = Item.where(category: "フェイスパウダー").page(params[:page]).order("created_at DESC").per(25)
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :brand, :detail, :category, :evaluation, :age, :skin_color, :skin_condition, item_images_attributes: [:image]).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'newitems', to: 'items#newitem'
   get 'makeupbase', to: 'items#makeupbase'
   get 'concealer' , to: 'items#concealer'
+  get 'facepowder', to: 'items#facepowder'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
