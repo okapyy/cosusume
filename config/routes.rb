4f0b2c9ed64bcfa86fd34462aea2e10c5a74c22c
okapyy <m9_17g_suki@yahoo.co.jp>
2020-08-27 20:14:41 +0900

    リップグロス一覧

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   get 'eyebrow', to: 'items#eyebrow'
   get 'lipstick', to: 'items#lipstick'
   get 'lipcare_cream', to: 'items#lipcare_cream'
+  get 'lipgloss', to: 'items#lipgloss'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
